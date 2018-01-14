97e9990920555da3adc14254f5b01e1fc807b57a
Russell <russ.e.owen@gmail.com>
2018-01-14 12:11:34 -0500

    Take Chapter 5 notes

--- scripts/Chap5.runsexpectancy.R
+++ scripts/Chap5.runsexpectancy.R
@@ -9,107 +9,19 @@
 ##################################################
 # 5.2  Runs Scored in the Remainder of the Inning
 ##################################################
-
-data2011 <- read.csv("all2011.csv", header=FALSE)
-fields <- read.csv("fields.csv")
-names(data2011) <- fields[, "Header"]
-
-# total runs at any point in the game 
-data2011$RUNS <- with(data2011, AWAY_SCORE_CT + HOME_SCORE_CT)
-
-# unique ID for each half inning 
-data2011$HALF.INNING <- with(data2011, 
-                            paste(GAME_ID, INN_CT, BAT_HOME_ID))
-
-# Compute the total runs scored in each half inning 
-data2011$RUNS.SCORED <- with(data2011, (BAT_DEST_ID > 3) +
-  (RUN1_DEST_ID > 3) + (RUN2_DEST_ID > 3) + (RUN3_DEST_ID > 3))
-
-RUNS.SCORED.INNING <- aggregate(data2011$RUNS.SCORED, 
-                        list(HALF.INNING = data2011$HALF.INNING), sum)
-
-# Find the total game runs at the beginning of each half inning 
-RUNS.SCORED.START <- aggregate(data2011$RUNS, 
-                       list(HALF.INNING = data2011$HALF.INNING), "[", 1)
-
-# Find the max score for each half inning (score @ beginning, plus score @ end), merge with master df 
-MAX <- data.frame(HALF.INNING=RUNS.SCORED.START$HALF.INNING)
-MAX$x <- RUNS.SCORED.INNING$x + RUNS.SCORED.START$x
-data2011 <- merge(data2011, MAX)
-N <- ncol(data2011)
-names(data2011)[N] <- "MAX.RUNS" # Names the new column (at the end)
-
-# (ROI = remainder of inning) Calcs the runs scores in the remainder of the inning, by subtracting runs scored 
-# during the play, from the runs scored by the end of the half inning 
-data2011$RUNS.ROI <- data2011$MAX.RUNS - data2011$RUNS
-
-##################################################
-# 5.3  Creating the Matrix
-##################################################
-
-get.state <- function(runner1, runner2, runner3, outs){
-  runners <- paste(runner1, runner2, runner3, sep="")
-  paste(runners, outs)                      
-}
-
-# Flag if bases 1,2, or 3 are occupied by a runner @ plate appearance, store as a var 
-# e.g. '011 2' means runners on 2 & 3, 2 outs 
-RUNNER1 <- ifelse(as.character(data2011[,"BASE1_RUN_ID"])=="", 0, 1)
-RUNNER2 <- ifelse(as.character(data2011[,"BASE2_RUN_ID"])=="", 0, 1)
-RUNNER3 <- ifelse(as.character(data2011[,"BASE3_RUN_ID"])=="", 0, 1)
-data2011$STATE <- get.state(RUNNER1, RUNNER2, RUNNER3, data2011$OUTS_CT) 
-
-# We only consider plays with a change in runners on base, # outs, or runs scored 
-# Flag if 1st, 2nd, or 3rd base are occupied after the play 
-NRUNNER1 <- with(data2011, as.numeric(RUN1_DEST_ID==1 | BAT_DEST_ID==1))
-NRUNNER2 <- with(data2011, as.numeric(RUN1_DEST_ID==2 | RUN2_DEST_ID==2 | BAT_DEST_ID==2))
-NRUNNER3 <- with(data2011, as.numeric(RUN1_DEST_ID==3 | RUN2_DEST_ID==3 |
-  RUN3_DEST_ID==3 | BAT_DEST_ID==3))
-NOUTS <- with(data2011, OUTS_CT + EVENT_OUTS_CT) # num of outs after the play 
-
-# Return the # runners on base, and outs, after the play 
-data2011$NEW.STATE <- get.state(NRUNNER1, NRUNNER2, NRUNNER3, NOUTS)
-
-# Subset the df, to only contain plays with state changes 
-data2011 <- subset(data2011, (STATE!=NEW.STATE) | (RUNS.SCORED>0))
-
-# Remove any "incomplete" innings with <3 outs (e.g., 9th inning ends early b/c of runs scored to win)
-library(plyr)
-data.outs <- ddply(data2011, .(HALF.INNING), summarize,
-                  Outs.Inning = sum(EVENT_OUTS_CT))
-data2011 <- merge(data2011, data.outs)
-data2011C <- subset(data2011, Outs.Inning == 3)
-
-# Compute the expected # runs scores in the rest of the inning 
-RUNS <- with(data2011C, aggregate(RUNS.ROI, list(STATE), mean))
-RUNS$Outs <- substr(RUNS$Group, 5, 5)
-RUNS <- RUNS[order(RUNS$Outs), ]
-
-# Create the matrix 
-RUNS.out <- matrix(round(RUNS$x, 2), 8, 3) # b/c it's sorted by outs, and there are 8 states, this puts the outs into cols
-dimnames(RUNS.out)[[2]] <- c("0 outs", "1 out", "2 outs")
-dimnames(RUNS.out)[[1]] <- c("000", "001", "010", "011", "100", "101", "110", "111")
-
-# For comparison, show the 2002 matrix 
-RUNS.2002 <- matrix(c(.51, 1.40, 1.14,  1.96, .90, 1.84, 1.51, 2.33,
-               .27,  .94,  .68,  1.36, .54, 1.18,  .94, 1.51,
-               .10,  .36,  .32,   .63, .23, .52,   .45, .78),
-               8, 3)
-dimnames(RUNS.2002) <- dimnames(RUNS.out)
-
-# The left side shows 2011, the right 2002: 
-# displays the avg # runs a team will score in the remainder of the half inning 
-cbind(RUNS.out, RUNS.2002)
-
+ 
 ##################################################
 # 5.4  Measuring Success of a Batting Play
 ##################################################
 
-RUNS.POTENTIAL <- matrix(c(RUNS$x, rep(0, 8)), 32, 1)
+# After a batting play, the "state" changes (runners on, num outs). The value of a batting play, is the 
+# difference between the change in expected runs by end of inning, and the actual runs produced by the play. 
+
+RUNS.POTENTIAL <- matrix(c(RUNS$x, rep(0, 8)), 32, 1) # creates a 32x1 matrix (merge-able)
 dimnames(RUNS.POTENTIAL)[[1]] <- c(RUNS$Group, "000 3","001 3",
                                    "010 3","011 3","100 3","101 3","110 3","111 3") 
-data2011$RUNS.STATE <- RUNS.POTENTIAL[data2011$STATE,]
-data2011$RUNS.NEW.STATE <- RUNS.POTENTIAL[data2011$NEW.STATE,]
+data2011$RUNS.STATE <- RUNS.POTENTIAL[data2011$STATE,] # merges the expected values
+data2011$RUNS.NEW.STATE <- RUNS.POTENTIAL[data2011$NEW.STATE,] # runs expectancy post-batting play 
 data2011$RUNS.VALUE <- data2011$RUNS.NEW.STATE - data2011$RUNS.STATE + 
   data2011$RUNS.SCORED
 
@@ -122,17 +34,20 @@ albert.id <- subset(Roster, First.Name == "Albert" &
                      Last.Name == "Pujols")$Player.ID
 albert.id <- as.character(albert.id)
 albert <- subset(data2011, BAT_ID==albert.id)
-albert <- subset(albert, BAT_EVENT_FL==TRUE)
+albert <- subset(albert, BAT_EVENT_FL==TRUE) # isolate events where Pujols was the hitter 
 
-albert[1:2, c("STATE", "NEW.STATE", "RUNS.VALUE")]
+albert[1:2, c("STATE", "NEW.STATE", "RUNS.VALUE")] # how were the first two plate appearances? 
 
+# Tabulate how often Pujols was batting with runners in scoring position, etc...
 albert$RUNNERS <- substr(albert$STATE, 1, 3)
 table(albert$RUNNERS)
 
+# Visualize how Pujols performed with runners in each of these states 
 with(albert, stripchart(RUNS.VALUE ~ RUNNERS, vertical=TRUE, jitter=0.2,
                         xlab="RUNNERS", method="jitter", pch=1, cex = 0.8))
 abline(h=0)
 
+# Compute the number of opportunities, and sum of runs, for Pujols in each of the states. 
 A.runs <- aggregate(albert$RUNS.VALUE, list(albert$RUNNERS), sum)
 names(A.runs)[2] <- "RUNS"
 A.PA <- aggregate(albert$RUNS.VALUE, list(albert$RUNNERS), length)
@@ -140,7 +55,8 @@ names(A.PA)[2] <- "PA"
 A <- merge(A.PA, A.runs)
 A
 
-sum(A$RUNS)
+# Calculate the total runs contribution for the season 
+sum(A$RUNS) # 27 doesn't mean much with no context.. so we'll compare this to how the other players did 
 
 ##################################################
 # 5.6  Opportunity and Success for All Hitters
@@ -148,6 +64,7 @@ sum(A$RUNS)
 
 data2011b <- subset(data2011, BAT_EVENT_FL == TRUE)
 
+# Calculate total plate appearances, run value, and run potential for each player in 2011 
 runs.sums <- aggregate(data2011b$RUNS.VALUE, list(data2011b$BAT_ID), sum)
 runs.pa <- aggregate(data2011b$RUNS.VALUE, list(data2011b$BAT_ID), length)
 runs.start <- aggregate(data2011b$RUNS.STATE, list(data2011b$BAT_ID), sum)
@@ -157,34 +74,41 @@ names(runs.start) <- c("Batter", "Runs.Start")
 runs <- merge(runs.sums, runs.pa)
 runs <- merge(runs, runs.start)
 
+# We don't want pitchers, or non-starters, so we restrict to batters with >=400 plate appearances 
 runs400 <- subset(runs, PA >=400)
 head(runs400)
 
+# Scatterplot of run opportunities, against run value: as expected, 
+# batters with more run opportunities produce higher value
 with(runs400, plot(Runs.Start, Runs))
 with(runs400, lines(lowess(Runs.Start, Runs)))
 abline(h=0)
 
+# Restrict to only the batters who created more than 40 runs 
 runs400.top <- subset(runs400, Runs >=40)
 roster2011 <- read.csv("roster2011.csv")  
 runs400.top <- merge(runs400.top, 
               roster2011, by.x="Batter", by.y="Player.ID")
-with(runs400.top, text(Runs.Start, Runs, Last.Name, pos=1))
+with(runs400.top, text(Runs.Start, Runs, Last.Name, pos=1)) # label those points on the plot 
 
 ##################################################
 # 5.7  Position in the Batting Lineup
 ##################################################
 
+# Find the batting position they had the most frequently, for each player 
 get.batting.pos <- function(batter){
   TB <- table(subset(data2011,BAT_ID==batter)$BAT_LINEUP_ID)
   names(TB)[TB==max(TB)][1]}
 
 position <- sapply(as.character(runs400$Batter), get.batting.pos)
 
-with(runs400, plot(Runs.Start, Runs, type="n"))
+# Plot the run values, against potential, using batting position 
+with(runs400, plot(Runs.Start, Runs, type="n")) # type="n" plots just the axes, without point (we use text() below)
 with(runs400, lines(lowess(Runs.Start, Runs)))
 abline(h=0)
-with(runs400, text(Runs.Start, Runs, position))
+with(runs400, text(Runs.Start, Runs, position)) # this plots the batting position, in lieu of points 
 
+# How does Pujols stack up in this scatterplot? 
 AP <- subset(runs400, Batter==albert.id)
 points(AP$Runs.Start, AP$Runs, pch=19, cex=3)
 
@@ -192,29 +116,36 @@ points(AP$Runs.Start, AP$Runs, pch=19, cex=3)
 # 5.8 Run Values of Different Base Hits
 ##################################################
 
-d.homerun <- subset(data2011, EVENT_CD==23)
+#### Calculate run value of a home run ####
+d.homerun <- subset(data2011, EVENT_CD==23) # dataset with only the homeruns 
 
-table(d.homerun$STATE)
+table(d.homerun$STATE) # tabulate runner states for each HR 
 
-round(prop.table(table(d.homerun$STATE)), 3)
+round(prop.table(table(d.homerun$STATE)), 3) # display relative frequencies, rounded to 3 decimal places 
+# over half of HRs are hit with no runners on base 
 
+# Histogram of run values, for all HRs 
 library(MASS)
-truehist(d.homerun$RUNS.VALUE)
+truehist(d.homerun$RUNS.VALUE) # most homeruns have run value of 1 (i.e. bases empty)
 
+# Which runners/out situations lead to the most valuable homeruns? 
+# Extract the play with the largest run value 
 subset(d.homerun, RUNS.VALUE==max(RUNS.VALUE))[1, 
-      c("STATE", "NEW.STATE", "RUNS.VALUE")]
+      c("STATE", "NEW.STATE", "RUNS.VALUE")] # bases loaded with 2 outs 
 
+# Overall run value of a homerun: the mean run value 
 mean.HR <- mean(d.homerun$RUNS.VALUE)
 mean.HR
 
-abline(v = mean.HR, lwd=3)
+abline(v = mean.HR, lwd=3) # draw the mean value on the histogram 
 text(1.5, 5, "Mean Runs Value", pos=4)
 
+#### Calculate the run value of a single #### 
 d.single <- subset(data2011, EVENT_CD == 20)
 #library(MASS)
 truehist(d.single$RUNS.VALUE)
 
-table(d.single$STATE)
+table(d.single$STATE) # most singles occur with the bases empty 
 
 subset(d.single, d.single$RUNS.VALUE==
   max(d.single$RUNS.VALUE))[, c("STATE", "NEW.STATE", "RUNS.VALUE")]
@@ -231,22 +162,23 @@ text(.5, 5, "Mean Runs Value", pos=4)
 # 5.9  Value of Base Stealing
 ##################################################
 
-stealing <- subset(data2011, EVENT_CD==6 | EVENT_CD==4)
+stealing <- subset(data2011, EVENT_CD==6 | EVENT_CD==4) # 6 == "Caught Stealing", 4 == "Stolen base" 
 
 table(stealing$EVENT_CD)
 
-table(stealing$STATE)
+table(stealing$STATE) # most common runner/out states for attempting a stolen base? 
 
 #library(MASS)
 truehist(stealing$RUNS.VALUE)
 
+# Isolate to situations with 1 out, runner on first 
 stealing.1001 <- subset(stealing, STATE=="100 1")
 
 table(stealing.1001$EVENT_CD)
 
 with(stealing.1001, table(NEW.STATE))
 
-mean(stealing.1001$RUNS.VALUE)
+mean(stealing.1001$RUNS.VALUE) # stolen bases are worthwhile, though the effect is minimal 
 
 #######################################################
 
